4da977402667cae16bcf23463b4e1f7f220827b7
Jiaxin Lin <craiglin1992@gmail.com>
2015-07-20 01:13:51 -0500

    something more

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,30 +5,31 @@ from .models import Question
 from .models import Choice
 
 
-#add inline to the question view
-#admin.TabularInline takes less space (each as one line)
+# add inline to the question view
+# admin.TabularInline takes less space (each as one line)
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
-#this is choosing how the admin form want to display
-#could either just have a list of fields or fieldsets with
+
+# this is choosing how the admin form want to display
+# could either just have a list of fields or fieldsets with
 #   categorized fields
 # collapse is to collapse that fieldset for now
 class QuestionAdmin(admin.ModelAdmin):
-    #this is for displaying in a table manner, each is a horizontal list
+    # this is for displaying in a table manner, each is a horizontal list
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'],'classes': ['collapse']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    #add a filter
+    # add a filter
     list_filter = ['pub_date']
-    #add a search field
+    # add a search field
     search_fields = ['question_text']
 
 
-#to register for the model so that admin can add records
+# to register for the model so that admin can add records
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
