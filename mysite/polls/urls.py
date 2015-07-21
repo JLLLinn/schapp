06121c57a2fd342b2191e5354921bc4007c09e2b
Jiaxin Lin <craiglin1992@gmail.com>
2015-07-21 01:33:18 -0500

    Added view amended

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,13 +4,22 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    # the name of ur here is for reference back to the url in template
+    # the name of url here is for reference back to the url in template
     # find the back reference in polls/index.html
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # now we amend it to suit our needs
+    # note that here it is a generic view that we are using
+    #   as refer back to the views file
+    #   also we are using pk instead of question_id instead
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,45 +5,76 @@ from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-# Create your views here.
-# each view is only responsible for two things
-# return an HttpResponse or raise exception
+# # Create your views here.
+# # each view is only responsible for two things
+# # return an HttpResponse or raise exception
+#
+# # this uses the template
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     # context = RequestContext(request, {
+#     #    'latest_question_list': latest_question_list,
+#     # })
+#     # return HttpResponse(template.render(context))
+#
+#     # These 2 lines have taken over the former 2 lines
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     # if the question_id doesn't exist, it raises a 404 exception
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#
+#     # instead of capturing the DoesNotExist exception, which exist at the model layer
+#     # we use this shortcut "get_object_or_404" that decouple the two layers.
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
-# this uses the template
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = RequestContext(request, {
-    #    'latest_question_list': latest_question_list,
-    # })
-    # return HttpResponse(template.render(context))
+# The following is from tutorial part 4
+# The above part is no longer in use, we now use the following part with generic view class
+class IndexView(generic.ListView):
+    # SO it seems like the context for those generic view can only hold one variable?
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    # These 2 lines have taken over the former 2 lines
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # if the question_id doesn't exist, it raises a 404 exception
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
+# python class definition:
+# class DerivedClassName(BaseClassName)
+class DetailView(generic.DetailView):
+    # by specifying model, Question is in our context,
+    # How how does it know which question?
+    # does it fucking know that pk is the id of the question and just fking get that question???
+    # ok so it does. WHen "pk" is changed, it gives me the error saying:
+    #   "Generic detail view DetailView must be called with either an object pk or a slug."
+    # it fetches the row from pk
+    model = Question
 
-    # instead of capturing the DoesNotExist exception, which exist at the model layer
-    # we use this shortcut "get_object_or_404" that decouple the two layers.
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question': question})
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 # This handles the vote form's post stuff
